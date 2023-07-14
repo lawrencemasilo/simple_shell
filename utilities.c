650d944b5390f1a9bb7b259685d2b3970e455def
Simamnkele <smambo85@gmail.com>
2023-07-14 16:21:30 +0200

    Created _strdup()

--- utilities.c
+++ utilities.c
@@ -25,3 +25,32 @@ char *_strcpy(char *dest, char *src)
 
 	return (dest);
 }
+
+/**
+ * _strdup - returns pointer to new string which is a copy of str
+ * @str: string to be duplicated
+ * Return: pointer to duplicate string on success, NULL on failure
+ */
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	ptr = malloc(j + 1 * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	while (i < j)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	return (ptr);
+}
+
+
